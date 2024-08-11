548222bdc926c4fe68a2e9b9d94d8ccf1b78d888
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-08-11 08:43:23 +0200

    feat: add opacity option

--- scripts/win-terminal-background.sh
+++ scripts/win-terminal-background.sh
@@ -14,17 +14,19 @@ blur_img() {
 
     blur_sigma=$(echo "$json_file" | jq -r '.profiles.defaults.blurSigma // "10"')
     blur="0x$blur_sigma"
-    output="$temp_dir/$blur-$(basename "$bg_image_wsl_path")"
+    output="$temp_dir/$RANDOM-$blur-$(basename "$bg_image_wsl_path")"
     magick "$bg_image_wsl_path" -blur "$blur" "$output"
     output_win_path=$(wslpath -w "$output" | tr "\\" "/" 2> /dev/null)
-    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$output_win_path\"")
+
+    opacity=$(echo "$json_file" | jq -r '.profiles.defaults.backgroundImageOpacityWithBlur')
+    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$output_win_path\" | .profiles.defaults.backgroundImageOpacity = $opacity")
     echo "$new_json" > "$win_terminal_settings"
 }
 
 if [[ $1 = "--increase-blur" ]]; then
     blur_sigma=$(echo "$json_file" | jq -r '.profiles.defaults.blurSigma // "10"')
     blur_sigma=$((blur_sigma + $2)) 
-    new_json=$(echo "$json_file" | jq ".profiles.defaults.blurSigma = \"$blur_sigma\"")
+    new_json=$(echo "$json_file" | jq ".profiles.defaults.blurSigma = $blur_sigma")
     json_file=$new_json
     blur_img
     exit 0
@@ -33,7 +35,7 @@ fi
 if [[ $1 = "--decrease-blur" ]]; then
     blur_sigma=$(echo "$json_file" | jq -r '.profiles.defaults.blurSigma // "10"')
     blur_sigma=$((blur_sigma - $2)) 
-    new_json=$(echo "$json_file" | jq ".profiles.defaults.blurSigma = \"$blur_sigma\"")
+    new_json=$(echo "$json_file" | jq ".profiles.defaults.blurSigma = $blur_sigma")
     json_file=$new_json
     blur_img
     exit 0
@@ -46,7 +48,8 @@ fi
 
 if [[ $1 = "--unblur" ]]; then
     bg_image_win_path=$(echo "$json_file" | jq -r '.profiles.defaults.backgroundImageOriginal')
-    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$bg_image_win_path\" | .profiles.defaults.backgroundImageOriginal = \"$bg_image_win_path\"")
+    opacity=$(echo "$json_file" | jq -r '.profiles.defaults.backgroundImageOpacityWithoutBlur')
+    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$bg_image_win_path\" | .profiles.defaults.backgroundImageOriginal = \"$bg_image_win_path\" | .profiles.defaults.backgroundImageOpacity = $opacity")
     echo "$new_json" > "$win_terminal_settings"
     exit 0
 fi
@@ -55,7 +58,8 @@ if [[ $1 = "--rotate" ]]; then
     dirs=$(echo "$json_file" | jq '.profiles.defaults.sources[]' | xargs -I{} wslpath {})
     new_random_image=$(find $dirs -type f 2>/dev/null | shuf -n 1 | xargs wslpath -w | tr "\\" "/" 2> /dev/null)
 
-    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$new_random_image\" | .profiles.defaults.backgroundImageOriginal = \"$new_random_image\"")
+    opacity=$(echo "$json_file" | jq -r '.profiles.defaults.backgroundImageOpacityWithoutBlur')
+    new_json=$(echo "$json_file" | jq ".profiles.defaults.backgroundImage = \"$new_random_image\" | .profiles.defaults.backgroundImageOriginal = \"$new_random_image\" | .profiles.defaults.backgroundImageOpacity = $opacity")
     echo "$new_json" > "$win_terminal_settings"
     exit 0
 fi
